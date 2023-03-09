0971adfa5f0104830f796173d571e22ba8965c71
AkshataChaubey <95972935+AkshataChaubey@users.noreply.github.com>
2023-03-09 20:03:28 +0530

    Add files via upload

---
+++ Move_File.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+from_dir="C:/Users/chaub/OneDrive/Desktop/Document_Files"
+to_dir="C:/Users/chaub/OneDrive/Desktop/Image_File"
+list_of_files=os.listdir(from_dir)
+print(list_of_files)
+for file_name in list_of_files:
+    name,extension=os.path.splitext(file_name)
+    print(extension) 
+    if extension=='':
+        continue
+    if extension in ['.gif','.png','.jpg','.jpeg','.jfif']:
+        path1=from_dir + '/'+file_name
+        path2=to_dir + '/' +"Image_File"
+        path3=to_dir + '/' +"Image_File"+'/'+ file_name
+    if os.path.exists(path2):
+        print("moving "+file_name+"......")
+        shutil.move(path1,path3)
+    else : os.makedirs(path2)
+    print("moving "+file_name+"......")
+    shutil.move(path1,path3)   
